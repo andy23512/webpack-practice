a2b9fdb89d32da26c8c038930c9f81c6e8bd0050
andy23512 <andy23512@gmail.com>
2018-02-20 11:06:11 +0800

    add sample image, use image in javascript

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import './style.scss';
+import codeURL from './code.png';
 
 import { groupBy } from 'lodash/collection';
 import people from './people';
@@ -7,3 +8,10 @@ const managerGroups = groupBy(people, 'manager');
 
 const root = document.querySelector('#root');
 root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`;
+
+const img = document.createElement('img');
+img.src = codeURL;
+img.style.backgroundColor = '#2B3A42';
+img.style.padding = '20px';
+img.width = 32;
+document.body.appendChild(img);
